c17d0b67f4efc0dedc545c8eb6fd5480186a0c02
James Orson <jamesaorson@gmail.com>
2024-12-21 14:22:20 -0800

    food: add red cabbage

--- templates/food/index.html.scm
+++ templates/food/index.html.scm
@@ -1,5 +1,14 @@
 (use-modules (scripts lib html))
 
+(define (recipe-list recipes)
+   `(ul ,(map-in-order (lambda (link)
+                           (let ([id (car link)]
+                                 [text (cdr link)])
+                              `(li (a (@ (id ,id)
+                                          (href ,id))
+                                       ,text))))
+         recipes)))
+
 (render-template "Ursinia - Food"
                  `((header
                      (div (@ (id "header"))
@@ -15,57 +24,39 @@
                    (div (h2 (a (@ (id "carnivore")
                                   (href "#carnivore"))
                                "Carnivore"))
-                        (ul ,(map-in-order (lambda (link)
-                                             (let ([id (car link)]
-                                                   [text (cdr link)])
-                                               `(li (a (@ (id ,id)
-                                                          (href ,(format #f "/~a" id)))
-                                                       ,text))))
-                                           '(["cheese-tortillas" . "Cheese Tortillas"]
-                                             ["chicken-burrito" . "Chicken Burrito"]
-                                             ["frozen-chicken-breast" . "[Air Fryer Hack] Frozen Chicken Breast"]
-                                             ["frozen-chicken-wings" . "[Air Fryer Hack] Frozen Chicken Wings"]))))
+                        ,(recipe-list '(["cheese-tortillas" . "Cheese Tortillas"]
+                                       ["chicken-burrito" . "Chicken Burrito"]
+                                       ["frozen-chicken-breast" . "[Air Fryer Hack] Frozen Chicken Breast"]
+                                       ["frozen-chicken-wings" . "[Air Fryer Hack] Frozen Chicken Wings"])))
                    (div (h2 (a (@ (id "omnivore")
                                   (href "#omnivore"))
                                "Omnivore"))
-                        (ul ,(map-in-order (lambda (link)
-                                             (let ([id (car link)]
-                                                   [text (cdr link)])
-                                               `(li (a (@ (id ,id)
-                                                          (href ,(format #f "/~a" id)))
-                                                       ,text))))
-                                           '(["banana-bread" . "Banana Bread"]
-                                             ["beef-chili" . "Beef Chili"]
-                                             ["biscuits" . "Biscuits"]
-                                             ["candy-cane-cookies" . "Candy Cane Cookies"]
-                                             ["cookie-dough-balls" . "Cookie Dough Balls"]
-                                             ["gingerbread-cookies" . "Gingerbread Cookies"]
-                                             ["instant-pot-spaghetti" . "Instant Pot Spaghetti"]
-                                             ["lasagna" . "Lasagna"]
-                                             ["macadamia-nut-cookies" . "Macadamia Nut Cookies"]
-                                             ["pbj-thumbprint-cookies" . "Peanut Butter and Jelly Thumbprint Cookies"]
-                                             ["pumpkin-pie" . "Pumpkin Pie"]
-                                             ["shepherds-pie" . "Shepherd's Pie"]))))
+                        ,(recipe-list '(["banana-bread" . "Banana Bread"]
+                                       ["beef-chili" . "Beef Chili"]
+                                       ["biscuits" . "Biscuits"]
+                                       ["candy-cane-cookies" . "Candy Cane Cookies"]
+                                       ["cookie-dough-balls" . "Cookie Dough Balls"]
+                                       ["gingerbread-cookies" . "Gingerbread Cookies"]
+                                       ["instant-pot-spaghetti" . "Instant Pot Spaghetti"]
+                                       ["lasagna" . "Lasagna"]
+                                       ["macadamia-nut-cookies" . "Macadamia Nut Cookies"]
+                                       ["pbj-thumbprint-cookies" . "Peanut Butter and Jelly Thumbprint Cookies"]
+                                       ["pumpkin-pie" . "Pumpkin Pie"]
+                                       ["shepherds-pie" . "Shepherd's Pie"])))
                    (div (h2 (a (@ (id "herbivore")
                                   (href "#herbivore"))
                                "Herbivore"))
-                        (ul ,(map-in-order (lambda (link)
-                                             (let ([id (car link)]
-                                                   [text (cdr link)])
-                                               `(li (a (@ (id ,id)
-                                                          (href ,(format #f "/~a" id)))
-                                                       ,text))))
-                                           '(["biscuits" . "Biscuits"]
-                                             ["cashew-cheese" . "Cashew Cheese"]
-                                             ["chanterelle-sauce" . "Chanterelle Sauce"]
-                                             ["chikn-alfredo" . "Chi'k'n Alfredo"]
-                                             ["chocolate-syrup" . "Chocolate Syrup"]
-                                             ["french-onion-soup" . "French Onion Soup"]
-                                             ["frozen-custard" . "Frozen Custard"]
-                                             ["mapo-tofu" . "Mapo Tofu"]
-                                             ["roasted-artichoke" . "Roasted Artichoke"]
-                                             ["spicy-fried-chicken-tofu-sandwich" . "Spicy Fried Chicken Tofu Sandwich"]
-                                             ["spinach-artichoke-dip" . "Spinach Artichoke Dip"]
-                                             ["st-lucia-buns" . "St. Lucia Buns"]
-                                             ["sugar-cookies" . "Sugar Cookies"]))))))
-
+                        ,(recipe-list '(["biscuits" . "Biscuits"]
+                                       ["cashew-cheese" . "Cashew Cheese"]
+                                       ["chanterelle-sauce" . "Chanterelle Sauce"]
+                                       ["chikn-alfredo" . "Chi'k'n Alfredo"]
+                                       ["chocolate-syrup" . "Chocolate Syrup"]
+                                       ["french-onion-soup" . "French Onion Soup"]
+                                       ["frozen-custard" . "Frozen Custard"]
+                                       ["mapo-tofu" . "Mapo Tofu"]
+                                       ["roasted-artichoke" . "Roasted Artichoke"]
+                                       ["spicy-fried-chicken-tofu-sandwich" . "Spicy Fried Chicken Tofu Sandwich"]
+                                       ["spinach-artichoke-dip" . "Spinach Artichoke Dip"]
+                                       ["st-lucia-buns" . "St. Lucia Buns"]
+                                       ["sugar-cookies" . "Sugar Cookies"]
+                                       ["red-cabbage" . "Swedish Brown Cabbage"])))))
