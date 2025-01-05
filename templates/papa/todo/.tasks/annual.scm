636576743bd3c055c14bd3e5b53fcb40e475cada
James Orson <jamesaorson@gmail.com>
2025-01-05 08:41:18 -0800

    Add some year tasks

--- templates/papa/todo/.tasks/annual.scm
+++ templates/papa/todo/.tasks/annual.scm
@@ -1,9 +1,19 @@
-'([title . "Year to Year"]
+'([title . "This year"]
   [tasks . (
    ([content . "File tax return"]
     [done? . #f])
    ([content . "Renew Subaru registration"]
-    [done? . #t])
+    [done? . #f])
    ([content . "Renew van registration"]
-    [done? . #t])
+    [done? . #f])
+   ([content . "Travel somewhere far"]
+    [done? . #f])
+   ([content . "Mama - Daily pullups"]
+    [done? . #f])
+   ([content . "Papa - Daily pullups"]
+    [done? . #f])
+   ([content . "Papa - Daily coding (less sabbath)"]
+    [done? . #f])
+   ([content . "Papa - Finish grad degree"]
+    [done? . #f])
 )])
