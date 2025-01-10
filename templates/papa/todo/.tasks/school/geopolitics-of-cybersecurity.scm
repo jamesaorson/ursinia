71e6c6eebd6aa62ad6dd22bc7d1f85916b914ab7
James Orson <jorson@purestorage.com>
2025-01-09 17:08:48 -0800

    Set up class projects for coming weeks

--- templates/papa/todo/.tasks/school/geopolitics-of-cybersecurity.scm
+++ templates/papa/todo/.tasks/school/geopolitics-of-cybersecurity.scm
@@ -3,20 +3,48 @@
                          "Module " ,module-number) " - Video " ,video-number)]
      [done? . ,done?]))
 
-(define (-render-module module-number module-url completed todo)
+(define (-render-assignment module-number module-url assignment done?)
+  (let ([name (car assignment)]
+        [date (cadr assignment)]
+        [href (caddr assignment)])
+       `([content . (span (a (@ (href ,module-url))
+                             "Module " ,module-number)
+                          "/"
+                          (a (@ (href ,href))
+                             ,name)
+                          ,(format #f " (Due: ~s)" date))]
+         [done? . ,done?])))
+
+(define (-render-module module-number module-url completed-videos todo-videos completed-assignments todo-assignments)
   `(,@(map-in-order (lambda (video)
                             (-render-video module-number module-url video #t))
-                    completed)
+                    completed-videos)
     ,@(map-in-order (lambda (video)
                             (-render-video module-number module-url video #f))
-                    todo)))
+                    todo-videos)
+    ,@(map-in-order (lambda (assignment)
+                      (-render-assignment module-number module-url assignment #t))
+                    completed-assignments)
+    ,@(map-in-order (lambda (assignment)
+                      (-render-assignment module-number module-url assignment #f))
+                    todo-assignments)))
 
 (define (module-1)
   (-render-module
     1
     "https://gatech.instructure.com/courses/442358/pages/module-1-instructor-video-content?module_item_id=4505530"
     `(1 2 3 4 5 6 7 8)
-    `()))
+    `()
+    `()
+    `(["Assignment 1 - Perusall Readings"
+       "February 2, 2025"
+       "https://gatech.instructure.com/courses/442358/assignments/1888008?module_item_id=4505534"]
+      ["Discussion 1"
+       "February 2, 2025"
+       "https://gatech.instructure.com/courses/442358/discussion_topics/1953103?module_item_id=4505536"]
+      ["Assignment 2 - Group Project - Case Overview 1 - Part 1"
+       "February 2, 2025"
+       "https://gatech.instructure.com/courses/442358/assignments/1888010?module_item_id=4505538"])))
 
 `([title . "Geopolitics of Cybersecurity: Spring 2025"]
   [tasks . [
--- templates/papa/todo/.tasks/school/intro-to-os.scm
+++ templates/papa/todo/.tasks/school/intro-to-os.scm
@@ -16,25 +16,30 @@
     ,@(-render
         `[
           "P1L1: Course Overview"
-        ]
-        `[
           "P1L2: Introduction to Operating Systems"
           "P2L1: Processes and Process Management"
+        ]
+        `[
+          "[Project 0] Setup lab environment"
           "P2L2: Threads and Concurrency"
           "P2L3: Threads Case Study: PThreads"
-          "Problem Set 1"
+          "[Project 1]"
           "P2L4: Thread Design Considerations"
           "P2L5: Thread Performance Considerations"
-          "Sample Midterm Exam Questions"
+          "[Midterm] Sample Questions"
+          "[Midterm] Exam"
           "P3L1: Scheduling"
           "P3L2: Memory Management"
           "P3L3: Inter-Process Communication"
           "P3L4: Synchronization Constructs"
           "P3L5: I/O Management"
           "P3L6: Virtualization"
+          "[Project 3] there is no project 2"
           "P4L1: Remote Procedure Calls"
           "P4L2: Distributed File Systems"
           "P4L3: Distributed Shared Memory"
+          "[Project 4]"
           "P4L4: Datacenter Technologies"
-          "Sample Final Questions"
+          "[Final] Sample Questions"
+          "[Final] Exam"
         ])]])
