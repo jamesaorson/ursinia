34be759fcb7d12172fc0704283c58d814a8618f7
James Orson <jorson@purestorage.com>
2025-01-13 11:06:21 -0800

    Change hierarchy

--- templates/papa/index.html.scm
+++ templates/papa/index.html.scm
@@ -13,22 +13,23 @@
                            (class "list-item-internal-link"))
                         "Papa"))
                    (div
+                     (h2 (a (@ (href "todo")
+                               (class "list-item-internal-link"))
+                            "Todo tracker")))
+                   (div
                      (img (@ (src "/shared/images/papa-bear.png")
                              (height 200)
                              (alt "Imposing Father Bear in Dramatic Mountain Landscape: An imposing father bear in a more rugged and dramatic mountain landscape. The terrain is harsher, with larger rocks and dramatic cliffs. The bear is mightier, with darker, more defined fur and a more powerful stance. His gaze is intensely protective, surveying the vast landscape under a stormy sky, adding to the wild, untamed nature of the scene."))))
                    (div
-                     (h2 (a (@ (href "todo")
-                                (class "list-item-internal-link"))
-                             "Papa's todo tracker"))
-                     (h2 "My " (a (@ (href "blog"))
+                     (h3 "My " (a (@ (href "blog"))
                                        "blog"))
-                     (h2 "My " (a (@ (href "devlog"))
+                     (h3 "My " (a (@ (href "devlog"))
                                        "devlog"))
                      (div
-                       (h2 "Check out " (a (@ (href "theology"))
+                       (h3 "Check out " (a (@ (href "theology"))
                                            "what I believe")))
                      (div
-                       (h2 "My library of content:")
+                       (h3 "My library of content:")
                        (ul
                          ,(map-in-order (lambda (link)
                                           (let ([href (car link)]
