22b93f1739b0de54e0487efc3f41404923262a36
James Orson <jamesaorson@gmail.com>
2025-02-01 15:55:55 -0800

    papa/todo: Finish P2L5

--- templates/papa/todo/.tasks/school/intro-to-os.scm
+++ templates/papa/todo/.tasks/school/intro-to-os.scm
@@ -34,10 +34,10 @@
           ,(-render-assignment "P2L4: Thread Design Considerations" "https://gatech.instructure.com/courses/431938/pages/p2l4-thread-design-considerations-playlist" "February 10, 2025")
           ,(-render-assignment "[Project 1/Part 2] multi-threaded gfclient" "https://gatech.instructure.com/courses/431938/assignments/1959418" "February 9, 2025")
           ,(-render-assignment "[Project 1/Part 2] multi-threaded gfserver" "https://gatech.instructure.com/courses/431938/assignments/1959420" "February 9, 2025")
+          ,(-render-assignment "P2L5: Thread Performance Considerations" "https://gatech.instructure.com/courses/431938/pages/p2l5-thread-performance-considerations-playlist" "February 17, 2025")
         )
         `(
           ,(-render-assignment "[Project 1/README]" "https://gatech.instructure.com/courses/431938/assignments/1959412" "February 9, 2025")
-          ,(-render-assignment "P2L5: Thread Performance Considerations" "https://gatech.instructure.com/courses/431938/pages/p2l5-thread-performance-considerations-playlist" "February 17, 2025")
           ["[Midterm] Sample Questions" "February 20, 2025"]
           ["[Midterm] Exam" "February 20-24, 2025"]
           ,(-render-assignment "P3L1: Scheduling" "https://gatech.instructure.com/courses/431938/pages/p3l1-scheduling-playlist" "February 24, 2025")
