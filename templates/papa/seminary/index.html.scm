212ce2621c1afe1ed4eb4d7e3a98b31a89cd8cb6
James Orson <jamesaorson@gmail.com>
2026-03-15 15:49:43 -0700

    theology: Finish homiletics
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/index.html.scm
+++ templates/papa/seminary/index.html.scm
@@ -13,7 +13,7 @@
                                    #\↤ " " (code "papa")))))
                    (main (@ (style "padding-bottom: 1rem;"))
                      (div (@ (style "float: left"))
-                        ,(render-list "courses/BS544-revelation"))
+                        ,(render-list "courses/PT501-homiletics"))
                      ; (div (@ (style "float: right"))
                      ;    ,(render-list "courses/AP605-survey_of_world_religions_and_cults"))
                         )))
