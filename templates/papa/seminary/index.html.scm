a556e6de3d86e7b543453bfe910d37b9bc73d3cf
James Orson <jamesaorson@gmail.com>
2026-03-15 15:41:43 -0700

    theology: Finish NT epistles
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/index.html.scm
+++ templates/papa/seminary/index.html.scm
@@ -13,7 +13,7 @@
                                    #\↤ " " (code "papa")))))
                    (main (@ (style "padding-bottom: 1rem;"))
                      (div (@ (style "float: left"))
-                        ,(render-list "courses/BS542-the_gospels_and_acts"))
+                        ,(render-list "courses/BS543-new_testament_epistles"))
                      ; (div (@ (style "float: right"))
                      ;    ,(render-list "courses/AP605-survey_of_world_religions_and_cults"))
                         )))
