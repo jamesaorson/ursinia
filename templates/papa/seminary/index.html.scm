dba0d18e82cdc962db26df67df7c8947dd99274f
James Orson <jamesaorson@gmail.com>
2026-03-15 15:12:30 -0700

    theology: Finish hermeneutics transcription
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/index.html.scm
+++ templates/papa/seminary/index.html.scm
@@ -13,7 +13,7 @@
                                    #\↤ " " (code "papa")))))
                    (main (@ (style "padding-bottom: 1rem;"))
                      (div (@ (style "float: left"))
-                        ,(render-list "courses/transcribed/BS502-hermeneutics"))
+                        ,(render-list "courses/BS502-hermeneutics"))
                      ; (div (@ (style "float: right"))
                      ;    ,(render-list "courses/AP605-survey_of_world_religions_and_cults"))
                         )))
