9d6afee28f2e4f648a7e5976fa5e0ccd72378f27
James Orson <jamesaorson@gmail.com>
2026-03-15 15:18:38 -0700

    theology: Transcribe OT history
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/.tasks/courses/BS531-old_testament_history_and_theology.scm
+++ templates/papa/seminary/.tasks/courses/BS531-old_testament_history_and_theology.scm
@@ -3,8 +3,52 @@
 (define todo
   (list
     (course/render-assignment
-      "Assignment name"
-      "https://example.com/assignment-link"
+      "Listen, outline, and take notes: Old Testament Survey 1-56 (Dr. Jack B. Scott, 45h)"
+      "https://www.sermonaudio.com/series/35860"
+      "...")
+    (course/render-assignment
+      "Listen, outline, and take notes: Old Testament History and Theology 1-13 (Dr. Dan Kim, 18h)"
+      "https://resources.covenantseminary.edu/programs/old-testament-history-theology"
+      "...")
+    (course/render-assignment
+      "Read and summarize: Is the Old Testament Reliable? (Brian Edwards)"
+      "https://answersingenesis.org/is-the-bible-true/is-the-old-testament-reliable/"
+      "...")
+    (course/render-assignment
+      "Read and summarize: A Scientific Investigation of the Old Testament (Robert Dick Wilson, 200pp)"
+      "https://www.bbfchurch.net/SiteData/bbf/A-Scientific-Investigation-of-the-OT-Robert-Dick-Wilson.pdf"
+      "...")
+    (course/render-assignment
+      "Read and summarize: Is the Higher Criticism Scholarly? (Robert Dick Wilson, 74pp)"
+      "https://archive.org/details/ishighercriticis00wils"
+      "...")
+    (course/render-assignment
+      "Read and summarize: Does Archaeology Support the Bible? (Clifford Wilson, The New Answers Book, ch. 25)"
+      "https://answersingenesis.org/archaeology/does-archaeology-support-the-bible/"
+      "...")
+    (course/render-assignment
+      "Read and summarize: Dead Sea Scrolls: Timeless Treasures from Qumran (Jeremy Lyon)"
+      "https://kidsanswers.org/kids-feedback-dead-sea-scrolls/" ; NOTE: This seems wrong...
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: The Church During the Old Testament Dispensation (C.C. Jones, 558pp)"
+      "https://archive.org/details/historyofchurcho00jone/page/n8/mode/1up"
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: The Typology of Scripture, Vol. 1 (Patrick Fairbairn)"
+      "https://archive.org/details/typologyofscript01fairiala"
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: The Typology of Scripture, Vol. 2 (Patrick Fairbairn)"
+      "https://archive.org/details/typologyofscript02fairiala"
+      "...")
+    (course/render-assignment
+      "Write: Introduction for each Old Testament book"
+      "https://example.com"
+      "...")
+    (course/render-assignment
+      "Write: 20-page paper on a subject of interest presented in the course material"
+      "https://example.com"
       "...")
   ))
 
