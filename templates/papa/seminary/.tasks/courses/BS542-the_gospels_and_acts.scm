bd16f1b55dfdbe0e9d11817af93a468acb6732d3
James Orson <jamesaorson@gmail.com>
2026-03-15 15:38:47 -0700

    theology: Finish gospels and acts
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/.tasks/courses/BS542-the_gospels_and_acts.scm
+++ templates/papa/seminary/.tasks/courses/BS542-the_gospels_and_acts.scm
@@ -3,8 +3,32 @@
 (define todo
   (list
     (course/render-assignment
-      "Assignment name"
-      "https://example.com/assignment-link"
+      "Read: Matthew, Mark, Luke, John, Acts"
+      "https://example.com"
+      "...")
+    (course/render-assignment
+      "Listen, outline, and take notes: The Gospels 1-10 (IIIM, 17h)"
+      "https://reformedseminary.org/course.asp/vs/gos"
+      "...")
+    (course/render-assignment
+      "Listen, outline, and take notes: The Gospels 1-40 (Dr. Robert Cara, 25h)"
+      "https://drive.google.com/file/d/1LgyqUPu72GtbkqyzJOPnpFTz3oSpdYeC/view"
+      "...")
+    (course/render-assignment
+      "Listen, outline, and take notes: The Book of Acts 1-63 (Dr. R.C. Sproul, 25h)"
+      "https://learn.ligonier.org/series/book-of-acts"
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: How to Study the New Testament: The Gospels and Acts of the Apostles (Henry Alford, 377pp)"
+      "https://archive.org/details/howtostudynewte00alfogoog"
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: The Teaching of Jesus Concerning the Kingdom of God and the Church (Geerhardus Vos, 190pp)"
+      "https://archive.org/details/theteachingofjes00vosuoft"
+      "...")
+    (course/render-assignment
+      "Write: Two 15-page papers on subjects of interest presented in the course material"
+      "https://example.com"
       "...")
   ))
 
