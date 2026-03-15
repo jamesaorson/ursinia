13243202e2e5b757f9d731970d8a62aa91410f27
James Orson <jamesaorson@gmail.com>
2026-03-15 15:12:58 -0700

    theology: Start OT history
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/index.html.scm
+++ templates/papa/seminary/index.html.scm
@@ -13,7 +13,7 @@
                                    #\↤ " " (code "papa")))))
                    (main (@ (style "padding-bottom: 1rem;"))
                      (div (@ (style "float: left"))
-                        ,(render-list "courses/BS502-hermeneutics"))
+                        ,(render-list "courses/BS531-old_testament_history_and_theology"))
                      ; (div (@ (style "float: right"))
                      ;    ,(render-list "courses/AP605-survey_of_world_religions_and_cults"))
                         )))
