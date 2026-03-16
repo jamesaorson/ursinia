2fef4ebe03c9a5454724a61559e68d7837d23eeb
James Orson <jamesaorson@gmail.com>
2026-03-15 22:45:47 -0700

    theology: Presby church history donex
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/.tasks/courses/CH527-presbyterian_church_history.scm
+++ templates/papa/seminary/.tasks/courses/CH527-presbyterian_church_history.scm
@@ -3,8 +3,20 @@
 (define todo
   (list
     (course/render-assignment
-      "Assignment name"
-      "https://example.com/assignment-link"
+      "Listen, outline, and take notes: American Presbyterianism 1-19 (Dr. Don Fortson, 15h)"
+      "https://drive.google.com/file/d/14JPX5PTmxQ2TnKIbl3N_k2MbWvJq7ISY/view"
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: History of the Presbyterian Churches of the World (R.C. Reed, 428pp)"
+      "https://archive.org/details/historyofpresbyt1915reed/page/n5/mode/2up"
+      "...")
+    (course/render-assignment
+      "Read and provide chapter summaries: History of Presbyterianism in All Ages (Robert Kerr, 312pp)"
+      "https://archive.org/details/peopleshistoryof00kerr"
+      "...")
+    (course/render-assignment
+      "Write: 20-page paper on a subject of interest presented in the course materials"
+      "https://example.com"
       "...")
   ))
 
