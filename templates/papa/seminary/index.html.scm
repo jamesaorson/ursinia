95797c65b27862cd3c00dae6fa82e5916077aecb
James Orson <jamesaorson@gmail.com>
2026-03-15 23:23:47 -0700

    theology: Render pastoral theology
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/index.html.scm
+++ templates/papa/seminary/index.html.scm
@@ -13,7 +13,7 @@
                                    #\↤ " " (code "papa")))))
                    (main (@ (style "padding-bottom: 1rem;"))
                      (div (@ (style "float: left"))
-                        ,(render-list "courses/TH630-contemporary_theology"))
+                        ,(render-list "courses/PT600-pastoral_theology"))
                      ; (div (@ (style "float: right"))
                      ;    ,(render-list "courses/AP605-survey_of_world_religions_and_cults"))
                         )))
