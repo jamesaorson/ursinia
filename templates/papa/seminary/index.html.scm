f25923a82480d2e292634b3c1c29af85d7b0dfab
James Orson <jamesaorson@gmail.com>
2026-03-15 22:48:39 -0700

    theology: Finish biblical ethics
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- templates/papa/seminary/index.html.scm
+++ templates/papa/seminary/index.html.scm
@@ -13,7 +13,7 @@
                                    #\↤ " " (code "papa")))))
                    (main (@ (style "padding-bottom: 1rem;"))
                      (div (@ (style "float: left"))
-                        ,(render-list "courses/CH527-presbyterian_church_history"))
+                        ,(render-list "courses/TH625-biblical_ethics"))
                      ; (div (@ (style "float: right"))
                      ;    ,(render-list "courses/AP605-survey_of_world_religions_and_cults"))
                         )))
